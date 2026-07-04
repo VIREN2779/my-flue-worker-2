0133cb9d2796258541f573af680e729e016392ef
VIREN2779 <virenpatadiya073@gmail.com>
2026-07-04 12:07:39 +0530

    fix(index.js): correct response message punctuation in fetch handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default {
     async fetch(request, env, ctx) {
-        return new Response("VIREN PATADIYA from this side...", {
+        return new Response("VIREN PATADIYA from this side!", {
             headers: { "content-type": "text/plain" },
         });
     },
